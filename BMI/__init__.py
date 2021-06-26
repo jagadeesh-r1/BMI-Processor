7002593d5f43fa12f3e5a94740ded88d25d53de9
jagadeeshEunimart <jagadeesh.cloudcontrol@gmail.com>
2021-06-26 13:17:33 +0530

    added setup.py

---
+++ BMI/__init__.py
@@ -0,0 +1,1 @@
+__author__='Jagadeesh Reddy'
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bmi_batch_process',
+    url='https://github.com/jagadeesh-r1/code-20210626-jagadeeshreddy',
+    author='Jagadeesh Reddy',
+    author_email='mail4jagadeeshreddy@gmail.com',
+    packages=['BMI'],
+    version='0.1',
+    license='GNU General Public License v3.0',
+    description='This package is used to process BMI (BodyMassIndex) using Height and Weight of person. The input must a iterable of json objects',
+    long_description=open('README.md').read()
+)
